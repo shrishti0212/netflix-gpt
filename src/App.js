13ecf04309a5cd6613c79a17800c41d1f25ba3a5
Shrishti Mishra <imshrishti0212@gmail.com>
2025-01-03 00:12:48 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="font-bold text-3xl from-neutral-700">
+     Namaste everyOne!!!
+    </div>
+  );
+}
+
+export default App;
